ddc631bb03be26b415dd3d8159a6aa39b39fb7f6
fishfishson <3150100814@zju.edu.cn>
2020-04-23 17:36:35 +0800

    init

--- options/base_options.py
+++ options/base_options.py
@@ -14,6 +14,7 @@ class BaseOptions:
         self.parser.add_argument('--lst_path', default='datasets/train_list.txt', help='path to dataset file list')
         self.parser.add_argument('--dataroot', default='datasets', help='path to model data dir')
         self.parser.add_argument('--dataset_mode', choices={"classification", "segmentation"}, default='segmentation')
+        self.parser.add_argument('--patch_size', type=list, default=[128, 128, 64])
         self.parser.add_argument('--ninput_edges', type=int, default=3000,
                                  help='# of input edges (will include dummy edges)')
         self.parser.add_argument('--max_dataset_size', type=int, default=float("inf"),
