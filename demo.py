c98df3f00cecf277174c80ded89cd9cfd4b37b46
fishfishson <3150100814@zju.edu.cn>
2020-04-23 14:49:05 +0800

    init

---
+++ demo.py
@@ -0,0 +1,30 @@
+from options.train_options import TrainOptions
+from data.dataloader import MSDSurfTrainDataset
+
+opts = TrainOptions().parse()
+dataset = MSDSurfTrainDataset(opts)
+import torch
+import numpy as np
+
+
+def collate_fn(batch):
+    """Creates mini-batch tensors
+    We should build custom collate_fn rather than using default collate_fn
+    """
+    meta = {}
+    keys = batch[0].keys()
+    print(keys)
+    for key in keys:
+        meta.update({key: np.array([d[key] for d in batch])})
+    return meta
+
+
+dataloader = torch.utils.data.DataLoader(dataset,
+                                         batch_size=2,
+                                         shuffle=False,
+                                         num_workers=2,
+                                         collate_fn=collate_fn)
+for i, data in enumerate(dataloader):
+    if i == 0:
+        break
+print(type(data))
